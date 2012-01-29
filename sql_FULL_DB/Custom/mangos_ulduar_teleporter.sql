05309e7e829ea9c2e7f4700f614bcc1d7bbcbc99
conan513 <conan_513@hotmail.com>
2012-01-29 09:16:50 +0100

    new beginning

---
+++ sql_FULL_DB/Custom/mangos_ulduar_teleporter.sql
@@ -0,0 +1,6 @@
+update gameobject_template set data10 = 29216 where entry = 194569;
+update spell_target_position set target_map = 603 where id = 29216;
+update spell_target_position set target_position_x = 1497.365723 where id = 29216;
+update spell_target_position set target_position_y = -24.363924 where id = 29216;
+update spell_target_position set target_position_z = 421.366669 where id = 29216;
+update spell_target_position set target_orientation = 0.104423 where id = 29216;
